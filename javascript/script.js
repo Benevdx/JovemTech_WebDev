47824c0c768739740b02a13b79c741e450b12f92
benevdx <victorbenevides4@gmail.com>
2026-03-30 10:49:43 -0300

    adição de atividades

---
+++ javascript/script.js
@@ -0,0 +1,28 @@
+// console.log(idade);
+// idade = 24;
+// console.log(idade);
+// const PI = 3.14;
+
+// const NOME = "Benevides";
+// console.log("meu nome é " + NOME);
+
+// let idade = 23;
+// console.log("eu tenho", idade + " anos");
+
+// let altura = 1.79;
+// console.log(`minha altura é ${altura}m`);
+
+// const gatos = 1;
+// console.log(`eu tenho ${gatos} gato e 1 coelho`);
+
+// let u = 2;
+// let i = 3;
+
+// console.log(u + i);
+// console.log(u - i);
+// console.log(u * i);
+// console.log(u / i);
+// console.log(u ** i);
+// console.log(i % u);
+
+console.log("5" - Infinity);
